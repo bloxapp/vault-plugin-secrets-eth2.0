5fb11d6dc3a677ed2afb28c319720c3dc52de8b1
Roman Behma <begmaroman@gmail.com>
2020-11-25 19:55:07 +0200

    improved errors reporting

--- utils/httpex/client.go
+++ utils/httpex/client.go
@@ -19,12 +19,13 @@ const (
 )
 
 // CreateClient creates a new HTTP client.
-func CreateClient(logger *logrus.Entry) *http.Client {
+func CreateClient(logger *logrus.Entry, errorHandler retryablehttp.ErrorHandler) *http.Client {
 	retryClient := retryablehttp.NewClient()
 	retryClient.RetryMax = attempts
 	retryClient.RetryWaitMin = attemptsWaitMin
 	retryClient.RetryWaitMax = attemptsWaitMax
 	retryClient.Logger = logger
+	retryClient.ErrorHandler = errorHandler
 
 	client := retryClient.StandardClient()
 	client.Timeout = clientTimeout
